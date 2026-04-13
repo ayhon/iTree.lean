29ae054b24934c6978e1fc3ba163f4900b0981ab
ayhon <ayhonnick@gmail.com>
2026-04-13 13:55:24 +0200

    feat: add return type to IterTree interface

--- IterTree/Basic.lean
+++ IterTree/Basic.lean
@@ -1,22 +1,24 @@
 namespace IterTree
 
 /-- iTree equivalent of Std.IterM -/
-structure iTree {σ : Type σ} («?» : Type r → Type q) (m : Type m₁ → Type m₂) : Type σ where
+structure iTree {σ : Type σ} («?» : Type r → Type q) (m : Type m₁ → Type m₂) (ν : Type ν) :
+    Type σ where
   internalState : σ
 
 -- NOTE: We have that ι = σ
 
 /-- iTree equivalent of Std.IterStep -/
-inductive Observation (ι : Type ι)(«?» : Type ι → Type q) : Type (max (ι+1) q) where
-  | ask {α : Type ι} (ev : «?» α) (it : α → ι) : Observation ι «?»
-  | skip (it : ι) : Observation ι «?»
-  | done : Observation ι «?»
+inductive Observation (ι : Type ι)(«?» : Type ι → Type q) (ν : Type ν) : Type _ where
+  | ask {α : Type ι} (ev : «?» α) (it : α → ι) : Observation ι «?» ν
+  | skip (it : ι) : Observation ι «?» ν
+  | done (v : ν) : Observation ι «?» ν
 
 /-- iTree equivalent of Std.Iterator -/
 class Executable (σ : Type σ)
   («?» : Type σ → Type q)
-  (m : Type (max (σ+1) q) → Type (max (σ+1) q)) 
+  (ν : Type ν)
+  (m : Type _ → Type _)
 where
-  IsPlausibleObservation : iTree (σ := σ) «?» m → Observation (iTree (σ := σ) «?» m) «?» → Prop
-  exec : (it : iTree (σ := σ) «?» m) →
-    m {obs : Observation (iTree (σ := σ) «?» m) «?» // IsPlausibleObservation it obs}
+  IsPlausibleObservation : iTree (σ := σ) «?» m ν → Observation (iTree (σ := σ) «?» m ν) «?» ν → Prop
+  exec : (it : iTree (σ := σ) «?» m ν) →
+    m {obs : Observation (iTree (σ := σ) «?» m ν) «?» ν // IsPlausibleObservation it obs}
